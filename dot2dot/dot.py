db61e1702ad8a05bc0237ae7668d841185fe8568
davidAlgis <david.algis@tutamail.com>
2024-12-13 23:34:06 +0100

    ♻️ Refactor dot based on pylint comment

--- dot2dot/dot.py
+++ dot2dot/dot.py
@@ -1,26 +1,58 @@
+"""
+Module to define the Dot class, representing a dot with associated properties and label.
+"""
+
 from dot2dot.dot_label import DotLabel
 
 
 class Dot:
     """
-    Class to represent a dot with associated properties and label.
+    Represents a dot with a position, unique ID, and associated label.
+
+    Attributes:
+        position (tuple): Coordinates of the dot as (x, y).
+        dot_id (int): Unique identifier for the dot.
+        color (tuple): RGBA color of the dot. Default is black.
+        radius (int): Radius of the dot. Default is 10.
+        label (DotLabel): Associated label for the dot.
+        overlap_other_dots (bool): Indicates if the dot overlaps with other dots.
+        overlap_dot_list (list): List of other dots this dot overlaps with.
+        overlap_label_list (list): List of labels overlapping with this dot.
     """
 
     def __init__(self, position, dot_id):
-        self.position = position  # Position of the dot (x, y)
-        self.dot_id = dot_id  # Unique ID for the dot
-        self.color = (0, 0, 0, 255)  # Dot color as RGBA tuple
-        self.radius = 10  # Radius of the dot
-        self.label = None  # Associated DotLabel object
+        """
+        Initializes a Dot instance.
+
+        Args:
+            position (tuple): Position of the dot (x, y).
+            dot_id (int): Unique identifier for the dot.
+        """
+        self.position = position
+        self.dot_id = dot_id
+        self.color = (0, 0, 0, 255)  # Default color: black
+        self.radius = 10  # Default radius
+        self.label = None
         self.overlap_other_dots = False
         self.overlap_dot_list = []
         self.overlap_label_list = []
 
     def set_label(self, color, font_path, font_size):
+        """
+        Sets a label for the dot using the DotLabel class.
+
+        Args:
+            color (tuple): RGBA color for the label text.
+            font_path (str): Path to the font file.
+            font_size (int): Size of the font.
+        """
         self.label = DotLabel(self.position, self.radius, color, font_path,
                               font_size, self.dot_id)
 
     def __repr__(self):
+        """
+        Returns a string representation of the Dot instance.
+        """
         return (
             f"Dot(position={self.position}, dot_id={self.dot_id}, color={self.color}, "
             f"radius={self.radius}, label={repr(self.label)})")
