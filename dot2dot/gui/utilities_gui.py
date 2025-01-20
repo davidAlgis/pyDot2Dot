adcfcd1d8747dfa0462b5be31c56406f526a5323
davidAlgis <david.algis@tutamail.com>
2025-01-20 21:44:44 +0100

    ✨ Add a way to manage the screen that is currently used by gui

--- dot2dot/gui/utilities_gui.py
+++ dot2dot/gui/utilities_gui.py
@@ -1,5 +1,6 @@
 import os
 from dot2dot.utils import get_base_directory
+from screeninfo import get_monitors
 
 
 def set_icon(window):
@@ -11,3 +12,31 @@ def set_icon(window):
         window.iconbitmap(icon_path)
     else:
         print(f"Warning: Icon not found at {icon_path}")
+
+
+def set_screen_choice(root, config):
+    # Screen choice
+    screen_choice = config["screenChoice"]
+    if screen_choice is None:
+        screen_choice = 0
+    monitors = get_monitors()
+    if screen_choice < len(monitors):
+        selected_monitor = monitors[screen_choice]
+        root.geometry(
+            f"{selected_monitor.width}x{selected_monitor.height}+{selected_monitor.x}+{selected_monitor.y}"
+        )
+    else:
+        print(
+            f"Invalid screen choice: {screen_choice}. Defaulting to main screen."
+        )
+
+
+def get_screen_choice(config):
+    screen_choice = config["screenChoice"]
+    if screen_choice is None:
+        return 0
+    monitors = get_monitors()
+    if screen_choice < len(monitors):
+        return screen_choice
+    else:
+        0
